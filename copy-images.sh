0ab7b5dbfbf4cecc5c689dbc0b0ee33dc0fdd7ec
root <root@bastion.ocp43.localdomain>
2020-03-24 22:06:49 -0500

    Version inicial de guía

--- copy-images.sh
+++ copy-images.sh
@@ -1,6 +1,23 @@
-          skopeo copy --format oci docker://quay.io/cephcsi/cephcsi:canary docker://registry.ocp4.localdomain.net:5000/cephcsi/cephcsi:canary
-          skopeo copy --format oci docker://quay.io/k8scsi/csi-attacher:v2.1.0 docker://registry.ocp4.localdomain.net:5000/k8scsi/csi-attacher:v2.1.0
-          skopeo copy --format oci docker://quay.io/k8scsi/csi-node-driver-registrar:v1.2.0 docker://registry.ocp4.localdomain.net:5000/k8scsi/csi-node-driver-registrar:v1.2.0
-          skopeo copy --format oci docker://quay.io/k8scsi/csi-provisioner:v1.4.0 docker://registry.ocp4.localdomain.net:5000/k8scsi/csi-provisioner:v1.4.0
-          skopeo copy --format oci docker://quay.io/k8scsi/csi-resizer:v0.4.0 docker://registry.ocp4.localdomain.net:5000/k8scsi/csi-resizer:v0.4.0
-          skopeo copy --format oci docker://quay.io/k8scsi/csi-snapshotter:v1.2.2 docker://registry.ocp4.localdomain.net:5000/k8scsi/csi-snapshotter:v1.2.2
+#!/bin/bash
+
+REGISTRY=${1:-registry.ocp43.localdomain:5000}
+
+#Auth to local registry
+podman login ${REGISTRY}
+
+if [ $? -ne 0 ]
+then
+  echo "Invalid login... leaving"
+fi
+
+echo "Copying images to local registry"
+
+skopeo copy --format oci docker://quay.io/cephcsi/cephcsi:canary docker://${REGISTRY}/cephcsi/cephcsi:canary
+skopeo copy --format oci docker://quay.io/k8scsi/csi-attacher:v2.1.0 docker://${REGISTRY}/k8scsi/csi-attacher:v2.1.0
+skopeo copy --format oci docker://quay.io/k8scsi/csi-node-driver-registrar:v1.2.0 docker://${REGISTRY}/k8scsi/csi-node-driver-registrar:v1.2.0
+skopeo copy --format oci docker://quay.io/k8scsi/csi-provisioner:v1.4.0 docker://${REGISTRY}/k8scsi/csi-provisioner:v1.4.0
+skopeo copy --format oci docker://quay.io/k8scsi/csi-resizer:v0.4.0 docker://${REGISTRY}/k8scsi/csi-resizer:v0.4.0
+skopeo copy --format oci docker://quay.io/k8scsi/csi-snapshotter:v1.2.2 docker://${REGISTRY}/k8scsi/csi-snapshotter:v1.2.2
+
+echo "Replacing image from manifests"
+sed -i "s/image: \(.*\)\/\(.*\)\/\(.*\)/image: $REGISTRY\/\2\/\3/g" */*.yaml
